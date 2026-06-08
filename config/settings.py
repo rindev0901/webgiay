882e7c71160b17c90ead353c766952215d8112da
rindev0901 <nguyenduc09012003@gmail.com>
2026-06-08 21:49:21 +0700

    update pass

--- config/settings.py
+++ config/settings.py
@@ -132,7 +132,7 @@ EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "nguyenduc09012003@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "scnn aqpn bpke tarq")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "scnnaqpnbpketarq")
 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
 SITE_URL = os.getenv("SITE_URL", "http://localhost:8000")
